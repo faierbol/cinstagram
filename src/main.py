810740092ebd4862212fd5e691660698c0ea5871
Demir Antay <demir99antay@gmail.com>
2019-09-21 14:38:58 +0300

    started adding the programmer panel aka version 1.1.0

---
+++ src/main.py
@@ -0,0 +1,11 @@
+'''
+
+    Title: Ceddit
+
+    Desc: This project is just a reddit clone. The reason I am not putting a
+          MIT license on this project because this is just an educational pro
+          -ject for me and I want it to stay it that way.
+
+    Author: Demir Antay (@demirantay) -- demir99antay@gmail.com
+
+'''
