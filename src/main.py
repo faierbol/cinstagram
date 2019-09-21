4f8d9b4a682525b62c54d6249c73edebfbaca454
Demir Antay <demir99antay@gmail.com>
2019-09-21 16:29:19 +0300

    started connecting views to the programmer panel ; g

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@
     Title: Ceddit
 
     Desc: This project is just a reddit clone. The reason I am not putting a
-          MIT license on this project because this is just an educational pro
-          -ject for me and I want it to stay it that way.
+          MIT license on the source code is because this is just an educational
+          project for me and I want it to stay it that way.
 
     Author: Demir Antay (@demirantay) -- demir99antay@gmail.com
 
@@ -14,10 +14,15 @@
 
 from flask import Flask, request, render_template, redirect, session
 
-# my own modules
+# views for url pathing
+from programmer_panel_app.views import programmer_panel_login
+from programmer_panel_app.views import programmer_panel_signup
+from programmer_panel_app.views import programmer_panel_dashboard
 
 
-# Configuration
+#######################################################################
+#                       Configuration                                 #
+#######################################################################
 app = Flask(
     __name__,
     static_folder="./static",
@@ -25,7 +30,25 @@ app = Flask(
 )
 
 app.secret_key = "change this in production"
+app.debug = True
+app.host = "127.0.0.1"
+app.port = 5000
 
 
+#######################################################################
+#                       URL Paths                                     #
+#######################################################################
+@app.route("/")
+def index():
+    return "hello"
 
-# URLS
+
+# Programming Panel url paths
+app.add_url_rule('/programmer_panel/login', 'programmer_panel_login', programmer_panel_login)
+app.add_url_rule('/programmer_panel/signup', 'programmer_panel_signup', programmer_panel_signup)
+app.add_url_rule('/programmer_panel/dashboard', 'programmer_panel_dashboard', programmer_panel_dashboard)
+
+
+# RUNNING THE APPLICATION
+if __name__ == "__main__":
+    app.run()
--- src/programmer_panel_app/views.py
+++ src/programmer_panel_app/views.py
@@ -5,15 +5,18 @@ def programmer_panel_login():
     '''
 
     '''
+    return 'programmer panel login'
 
 
 def programmer_panel_signup():
     '''
 
     '''
+    return 'programmer panel signup'
 
 
 def programmer_panel_dashboard():
     '''
 
     '''
+    return 'prgorammer panel dashboard'
