3de757a3d68b2b45c59e6cdb6f7665203ac33e41
Demir Antay <demir99antay@gmail.com>
2019-09-21 16:57:59 +0300

    started fixing the tempaltes and static files in programmer panel

--- src/programmer_panel_app/views.py
+++ src/programmer_panel_app/views.py
@@ -1,22 +1,43 @@
 # Views of the Programmer Panel App
 
-
-def programmer_panel_login():
-    '''
-
-    '''
-    return 'programmer panel login'
+from flask import Flask, request, render_template, redirect, session
 
 
 def programmer_panel_signup():
     '''
-
+        programmer panel signup: is the view where a new programmer to the
+        company can signup their accounts
     '''
-    return 'programmer panel signup'
+
+    # process the signup form
+
+    data = {
+
+    }
+    return render_template(
+        'programmer_panel/programmer_panel_signup.html', data=data
+    )
+
+
+def programmer_panel_login():
+    '''
+        programmer panel login: is the view where the programmer of the sites
+        can login to their accounts and view their programmer-admin page
+    '''
+
+    data = {
+
+    }
+    return 'programmer panel login'
 
 
 def programmer_panel_dashboard():
     '''
-
+        programmer panel dashboard is the landing page after a programmer
+        logins to their accounts.
     '''
+
+    data = {
+
+    }
     return 'prgorammer panel dashboard'
