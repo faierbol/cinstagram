f283873e8c5e7673a8c303d7d99e1428d1628dde
Demir Antay <demir99antay@gmail.com>
2019-11-14 04:45:33 +0300

    started adding more views to the apps

--- cinstagram/api_web/views.py
+++ cinstagram/api_web/views.py
@@ -5,10 +5,31 @@ from ..app import db
 
 
 # Blue prints
-admin_panel = Blueprint("admin_panel", __name__)
+api_web = Blueprint("api_web", __name__)
 
 
-@admin_panel.route("admin_panel/dashboard", methods=['POST', 'GET'])
-def admin_panel_dashboard():
+@api_web.route("/api_web", methods=['POST', 'GET'])
+def api_web_landing_page():
+    ''' ...  '''
+    # Deleting any sessions regarding top-tier type of users
 
-    return "This will be edited "
+    data = {}
+    return render_template("api_web/index_page.html", data=data)
+
+
+@api_web.route("/api_web/register", methods=['POST', 'GET'])
+def api_web_register():
+    ''' ...  '''
+    # Deleting any sessions regarding top-tier type of users
+
+    data = {}
+    return render_template("api_web/register_page.html", data=data)
+
+
+@api_web.route("/api_web/PostIdPath", methods=['POST', 'GET'])
+def api_web_post_page():
+    ''' ...  '''
+    # Deleting any sessions regarding top-tier type of users
+
+    data = {}
+    return render_template("api_web/post_page.html", data=data)
